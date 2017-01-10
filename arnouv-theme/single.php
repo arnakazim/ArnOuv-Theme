8abdd2d048d07e4cad3084dd166d3ce63fe9eeb6
Arnaud Ouvrier <arnaud.ouvrier@gmail.com>
2017-01-10 16:28:24 +0100

    Deleting post meta from single pages

--- arnouv-theme/single.php
+++ arnouv-theme/single.php
@@ -6,8 +6,6 @@
 				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?> 
 		  			<article class="blog-post">
 						<h2 class="blog-post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-						<p class="blog-post-meta"><?php echo __('Le', 'arnouv-theme').' ';  the_date(); echo ' '.__('par', 'arnouv-theme').' '; the_author_posts_link(); ?></p>
-
 						<?php the_content(); ?>
 					</article>
 
